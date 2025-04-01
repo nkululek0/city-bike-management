c2079854b475d5df9dfbbad070a685e69c9ad41b
nkululek0 <nkululekovuyo0103@gmail.com>
2025-04-01 12:49:53 +0200

    Completed dashboard user api endpoint

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,24 +1,22 @@
-import dashboardModel from "../models/dashbaord-model.js";
+import dashboardModel from "../models/dashboard-model.js";
 
 const dashboardController = {
     getDashboard (req, res) {
-        let errorMessage;
-
         try {
             let data = dashboardModel.getDashboard();
             
-            if (data) {
+            if (data && typeof data == "object" && Object.entries(data).length > 0) {
                 res.json(data);
             }
             else {
-                errorMessage = "There was an issue while proccessing dashboard data";
+                let errorMessage = "There was an issue while proccessing dashboard data";
                 throw new Error(errorMessage);
             }
         }
         catch (error) {
             res.status(502).json({
                 error: {
-                    message: error
+                    message: error.message
                 }
             });
         }
--- src/models/dashboard-model.js
+++ src/models/dashboard-model.js
@@ -2,11 +2,31 @@ const dashboardModel = {
     getDashboard () {
         let result = [];
 
-        result = [
-            {
-                
-            }
-        ];
+        result = {
+            cities: ["De Pijp", "Oud-Zuid", "Reguliersgracht", "Damrak"],
+            bikes: [
+                {
+                    code: 1,
+                    availability: "available",
+                    location: "De Pijp"
+                },
+                {
+                    code: 2,
+                    availability: "un-available",
+                    location: "Oud-Zuid"
+                },
+                {
+                    code: 3,
+                    availability: "available",
+                    location: "Reguliersgracht"
+                },
+                {
+                    code: 4,
+                    availability: "available",
+                    location: "Damrak"
+                }
+            ]
+        };
         return result;
     }
 };
